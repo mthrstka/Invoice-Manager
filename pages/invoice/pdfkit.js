37358c8f66622437dd54231b605fdca4def46fb1
Jared Weber <jaredweber54@gmail.com>
2023-04-12 11:03:52 -0500

    gets inputs from invoice

--- pages/invoice/pdfkit.js
+++ pages/invoice/pdfkit.js
@@ -1,10 +1,11 @@
 
 
+
+
 function getInput() {
   const customer = document.getElementById("select").value;
   const description = document.getElementById("descr").value;
   const quantity = document.getElementById("qty").value;
   const price = document.getElementById("price").value;
   const message = document.getElementById("msg").value;
-
 }
